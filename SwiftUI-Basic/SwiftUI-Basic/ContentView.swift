b94bde61070a0527af8630830d4e7f9e83cb0a81
eung7 <wannabeeung@gmail.com>
2022-08-22 09:02:29 +0900

    no message

---
+++ SwiftUI-Basic/SwiftUI-Basic/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-Basic
+//
+//  Created by 김응철 on 2022/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-Basic/SwiftUI-Basic/SwiftUI_BasicApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_BasicApp.swift
+//  SwiftUI-Basic
+//
+//  Created by 김응철 on 2022/08/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_BasicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
