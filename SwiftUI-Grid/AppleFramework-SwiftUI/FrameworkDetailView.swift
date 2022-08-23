a487c867f716e7a2d455ba4a3ff2f2ccda10f9c2
eung7 <wannabeeung@gmail.com>
2022-08-23 16:49:21 +0900

    SwiftUI Modal 이해하기 2

--- SwiftUI-Grid/AppleFramework-SwiftUI/FrameworkDetailView.swift
+++ SwiftUI-Grid/AppleFramework-SwiftUI/FrameworkDetailView.swift
@@ -8,26 +8,31 @@
 import SwiftUI
 
 struct FrameworkDetailView: View {
-  @Binding var framework: AppleFramework
-  
+  @Binding var framework: AppleFramework?
+//  @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+  @Binding var isShowingDetail: Bool
+
   var body: some View {
     VStack {
-      Image(framework.imageName)
-      Text(framework.name)
-      Text(framework.description)
+      if let framework = framework {
+        Image(framework.imageName)
+        Text(framework.name)
+        Text(framework.description)
+      } else {
+        Text("Nothing selected!")
+      }
       
       Button {
-        print("--> Tapped!")
+        isShowingDetail = false
       } label: {
         Text("Button")
       }
-
     }
   }
 }
 
 struct FrameworkDetailView_Previews: PreviewProvider {
   static var previews: some View {
-    FrameworkDetailView(framework: .constant(AppleFramework.list[0]))
+    FrameworkDetailView(framework: .constant(AppleFramework.list[0]), isShowingDetail: .constant(true))
   }
 }
--- SwiftUI-Grid/AppleFramework-SwiftUI/FrameworkListView.swift
+++ SwiftUI-Grid/AppleFramework-SwiftUI/FrameworkListView.swift
@@ -22,11 +22,18 @@ struct FrameworkListView: View {
         LazyVGrid(columns: layout) {
           ForEach($viewModel.models, id: \.self) { $item in
             FrameworkCell(framework: $item)
+              .onTapGesture {
+                viewModel.isShowingDetail = true
+                viewModel.selectedItem = item
+              }
           }
         }
         .padding([.top, .leading, .trailing], 16.0)
       }
       .navigationTitle("☀️ Apple Framework")
+    }
+    .fullScreenCover(isPresented: $viewModel.isShowingDetail) {
+      FrameworkDetailView(framework: $viewModel.selectedItem, isShowingDetail: $viewModel.isShowingDetail)
     }
   }
 }
